fdd3aea83fd0f865dc3b96503711512d7a8a2f5f
Beulah Bejado <79973098+beulah02@users.noreply.github.com>
2023-09-22 21:22:06 +0800

    add

--- js/main.js
+++ js/main.js
@@ -118,10 +118,16 @@
 })(jQuery);
 
 //modal
-function showModal() {
-  $("#modal").show();
-}
+const button = document.getElementById("popup-button");
+const overlay = document.getElementById("overlay");
+const popup = document.getElementById("popup");
 
-function hideModal() {
-  $("#modal").hide();
-}
+button.addEventListener("click", function () {
+  overlay.style.display = "block";
+  popup.style.display = "block";
+});
+
+overlay.addEventListener("click", function () {
+  overlay.style.display = "none";
+  popup.style.display = "none";
+});
